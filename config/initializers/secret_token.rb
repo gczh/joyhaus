8e523b446138bad6b25b5d1f44555809e17c9b2f
Gabriel Chuan <czh.gabriel@gmail.com>
2014-07-11 02:05:37 +0800

    setup rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JoyhausTmp::Application.config.secret_key_base = '41fcd392f9a37bc283320f442e9494bccf58e6097913f97a403453f8d2d43a5bfef4e000d6b2edc8ee7c70e73766d10144983e10b72d995453fca49a100a7345'
